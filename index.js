f1bef359874aaa18b90e4392fd4a8232cae40346
João Lemos <joao-pedro11@hotmail.com>
2021-10-09 15:18:05 -0300

    dotenv lib

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
+
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-
-const token = 'ODk2NDQ4NjA4NzQ2OTk1NzEy.YWHQ0w.Vfz1fT6aCW_pefPNBPGh3N4Jc1w';
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`)
@@ -11,5 +11,6 @@ bot.on('message',msg => {
     if (msg.content === 'Salve salve Família ...') {
         msg.reply('Salvee...')
     }
-})
-bot.login(token)
+});
+
+bot.login(process.env.TOKEN_DISCORD)
